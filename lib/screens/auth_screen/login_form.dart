7a4118198e88cd1eac8fb6defdb84ded2ea18c03
bobbywilliamskgh <bobbywilliam86@gmail.com>
2024-03-10 16:48:00 +0700

    update files

--- lib/screens/auth_screen/login_form.dart
+++ lib/screens/auth_screen/login_form.dart
@@ -26,7 +26,7 @@ class _LoginFormState extends State<LoginForm> {
       ));
     }
 
-    return BlocListener(
+    return BlocListener<LoginBloc, LoginState>(
       listener: (context, state) {
         if (state is LoginFailure) {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -37,31 +37,58 @@ class _LoginFormState extends State<LoginForm> {
           );
         }
       },
-      child: BlocBuilder(builder: ((context, state) {
-        return Padding(
-          padding: EdgeInsets.only(right: 20, left: 20, top: 80),
-          child: Form(
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  padding: EdgeInsets.only(bottom: 20, top: 40),
-                  child: const Text(
-                    'Quiz App Bahaso',
-                    style: TextStyle(
-                        color: Style.Colors.mainColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24),
+      child: BlocBuilder<LoginBloc, LoginState>(
+        builder: ((context, state) {
+          return Padding(
+            padding: EdgeInsets.only(right: 20, left: 20, top: 80),
+            child: Form(
+              child: Column(
+                children: [
+                  Container(
+                    height: 200,
+                    padding: EdgeInsets.only(bottom: 20, top: 40),
+                    child: const Text(
+                      'Quiz App Bahaso',
+                      style: TextStyle(
+                          color: Style.Colors.mainColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.bold),
-                  controller: _emailController,
-                  decoration: InputDecoration(
+                  SizedBox(height: 20),
+                  TextFormField(
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold),
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Style.Colors.mainColor),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Style.Colors.mainColor),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        contentPadding: EdgeInsets.only(left: 10, right: 10),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold),
+                    controller: _passwordController,
+                    decoration: InputDecoration(
                       prefixIcon: Icon(Icons.email),
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Style.Colors.mainColor),
@@ -72,79 +99,54 @@ class _LoginFormState extends State<LoginForm> {
                         borderRadius: BorderRadius.circular(25),
                       ),
                       contentPadding: EdgeInsets.only(left: 10, right: 10),
-                      labelText: 'Email',
+                      labelText: 'Password',
                       labelStyle: TextStyle(
                           fontSize: 12,
                           color: Colors.grey,
-                          fontWeight: FontWeight.w500)),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.bold),
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Style.Colors.mainColor),
-                      borderRadius: BorderRadius.circular(25),
+                          fontWeight: FontWeight.w500),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Style.Colors.mainColor),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    contentPadding: EdgeInsets.only(left: 10, right: 10),
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w500),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                state is LoginLoading
-                    ? CircularProgressIndicator()
-                    : TextButton(
-                        onPressed: _onLoginButtonPressed,
-                        child: Text(
-                          'LOG IN',
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  state is LoginLoading
+                      ? CircularProgressIndicator()
+                      : TextButton(
+                          onPressed: _onLoginButtonPressed,
+                          child: Text(
+                            'LOG IN',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
                         ),
-                      ),
-                Container(
-                  child: Row(
-                    children: [
-                      Text(
-                        'Tidak memiliki akun?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        'Daftar',
-                        style: TextStyle(
-                            color: Style.Colors.mainColor,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                  Container(
+                    child: Row(
+                      children: [
+                        Text(
+                          'Tidak memiliki akun?',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Daftar',
+                          style: TextStyle(
+                              color: Style.Colors.mainColor,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      })),
+          );
+        }),
+      ),
     );
   }
 }
